d94a3ff040d156b1ab639ead13a608aee83fc0a8
mirogohacki <mgohacki@uvm.edu>
2024-12-10 09:48:58 -0500

    made picture smaller

--- src/components/SpiralArrow.tsx
+++ src/components/SpiralArrow.tsx
@@ -18,42 +18,40 @@ const SpiralArrow = () => {
 
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 800 800"
-      className="absolute top-0 left-0 w-full h-full pointer-events-none"
-      aria-labelledby="spiralArrowTitle"
-      role="img"
-    >
-      <title id="spiralArrowTitle">Spiral Arrow Animation Pointing to Miro's Picture</title>
-      <path
-        ref={pathRef}
-        d="
-          M100,400
-          C100,300 700,300 700,400
-          C700,500 100,500 100,400
-          C100,300 700,300 700,400
-          C700,500 100,500 100,400
-          C100,300 700,300 700,400
-          L750,400
-        "
-        stroke="#6d28d9"
-        strokeWidth="4"
-        fill="none"
-        markerEnd="url(#arrowhead)"
-      />
-      <defs>
-        <marker
-          id="arrowhead"
-          markerWidth="10"
-          markerHeight="7"
-          refX="10"
-          refY="3.5"
-          orient="auto"
-        >
-          <polygon points="0 0, 10 3.5, 0 7" fill="#6d28d9" />
-        </marker>
-      </defs>
-    </svg>
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 100"
+    className="w-full h-full"
+    aria-labelledby="squigglyArrowTitle"
+    role="img"
+  >
+    <title id="squigglyArrowTitle">Squiggly Arrow from Top Right to Bottom Left</title>
+    <path
+      d="
+        M100,0
+        C90,15 80,0 70,15
+        S50,30 40,15
+        S30,0 20,15
+        S10,30 0,100
+      "
+      stroke="#6d28d9"
+      strokeWidth="2"
+      fill="none"
+      markerEnd="url(#arrowhead)"
+      className="squiggly-arrow"
+    />
+    <defs>
+      <marker
+        id="arrowhead"
+        markerWidth="10"
+        markerHeight="7"
+        refX="0"
+        refY="3.5"
+        orient="auto"
+      >
+        <polygon points="0 0, 10 3.5, 0 7" fill="#6d28d9" />
+      </marker>
+    </defs>
+  </svg>
   );
 };
 
